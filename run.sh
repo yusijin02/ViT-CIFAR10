6e8247b0e208d8be24a686b1f089e362c87b08ef
yusijin <1207937423@qq.com>
2023-06-29 08:58:11 +0000

    debug

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ BATCH_SIZE=256        # batch大小  # 64 acc=41
 PICTURE_SIZE=32       # CIFAR-10的图片是32x32的
 NUM_CLASSES=10        # CIFAR-10的图片是分10类的
 PATCH_SIZE=8          # 将一张图片打成8x8的小patch
-MLP_HIDDEN_SIZE=512   # Transformer内部的MLP的隐藏层维度
+MLP_HIDDEN_SIZE=256   # Transformer内部的MLP的隐藏层维度
 NUM_LAYER=4           # Transformer的层数
 STEPLR_STEP_SIZE=20   # 学习率多少个epoch下降一次
 
@@ -39,8 +39,8 @@ HIDDEN_SIZE=$((EMBEDDING_DIM * NUM_HEADS))        # Transformer块之间传递�
 ###################################################
 
 # 4 * 12
-EMBEDDING_DIM=16
-NUM_HEADS=6
+EMBEDDING_DIM=12
+NUM_HEADS=4
 HIDDEN_SIZE=$((EMBEDDING_DIM * NUM_HEADS))
 run
 
